bc8df91598ce44a96d12517a35b92a6e2ea267a4
Onaracs <brendan.scarano@gmail.com>
2014-06-30 19:47:20 -0400

    Added has_many associations to proper models

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,9 @@
 class Folder < ActiveRecord::Base
   validates :name, presence: true
 
+  has_many :user_folders
+  has_many :users, through: :user_folders
+
   has_many :folder_links
   has_many :links, through: :folder_links
 end
