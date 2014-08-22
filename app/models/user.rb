f33cbce6e811c7994bc3f3f8beac380042c408b4
Onaracs <brendan.scarano@gmail.com>
2014-08-22 08:54:57 -0400

    removed unncessary code from method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,18 +16,11 @@ class User < ActiveRecord::Base
   end
 
   def friends_using_app
-    graph = Koala::Facebook::GraphAPI.new(self.oauth_token)
-    # return graph.get_connections("me", "friends", api_version: 'v2.0')
-
-=begin
-  Possible code to get friends
-  
-=end
     friends_list = []
+    
+    graph = Koala::Facebook::GraphAPI.new(self.oauth_token)
     friends = graph.get_connections("me", "friends", api_version: 'v2.0')
-    p "=" * 30
-    p friends
-    p "=" * 30
+    
     friends.each do |friend|
       friends_list << User.find_by_uid(friend["id"])
     end
