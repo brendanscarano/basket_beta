defd1f7a6c671256a669f2d69fac961a1f6df1e3
Onaracs <brendan.scarano@gmail.com>
2014-08-24 17:27:29 -0400

    The sent_link method now creates a new shared link in the friends shared basket from the chrome extension

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -1,10 +1,12 @@
-class SharedLinksController < ApplciationController
+class SharedLinksController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
 
   def sent_link
-    p "=" * 30
-    p params
-    p current_user.id
-    p "=" * 30
+    friend_id = params["friendId"].to_i
+    shared_basket = SharedBasket.find(friend_id)
+    shared_basket.shared_links.create(sender_id: current_user.id,
+                                      url: params["url"])
+    redirect_to root_url
   end
 
 end
