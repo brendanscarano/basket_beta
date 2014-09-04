c70d84d1a2ee539e32157e91a611873de9f7a7ff
Onaracs <brendan.scarano@gmail.com>
2014-09-04 18:12:05 -0400

    Rails now accepts link info from the extension with a title, description, and image, and creates a link and folderlink with all of this info

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,20 +2,20 @@ class LinksController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def new_link
-    binding.pry
     #Should be find or create, to not duplicate urls in the database
     #include the new info being sent in from the extension
-    link = Link.find_by_url(params["url"]).first_or_create
+    binding.pry
+    link = Link.find_by_url(params["url"])
 
     if link == nil 
       link = Link.create(url: params["url"],
-                        title:,
-                        description:,
-                        image: )
+                        title: params["pageInfo"]["title"],
+                        description: params["pageInfo"]["description"],
+                        image: params["pageInfo"]["image"])
     end
-    
+
     FolderLink.create(link_id: link.id,
-                        folder_id: params["basketId"])
+                        folder_id: params["uniqueId"])
     redirect_to root_url
   end
 
