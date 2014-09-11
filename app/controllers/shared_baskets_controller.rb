144a5b227dbc043cf44447447ea72ce678578dcd
Onaracs <brendan.scarano@gmail.com>
2014-09-11 09:50:42 -0400

    added additional sections to the about page

--- app/controllers/shared_baskets_controller.rb
+++ app/controllers/shared_baskets_controller.rb
@@ -2,6 +2,8 @@ class SharedBasketsController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def show
+    @new_folder = Folder.new
+    
     shared_basket = SharedBasket.find(params[:id])
     @links = shared_basket.shared_link_array
   end
