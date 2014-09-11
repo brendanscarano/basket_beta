898467747eefb825faa57666cafd4b39043814ad
Onaracs <brendan.scarano@gmail.com>
2014-09-11 14:56:49 -0400

    added font awesome icons to time and date, and rearragned show

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -6,15 +6,15 @@ class SharedLinksController < ApplicationController
   end
 
   def sent_link
-    friend_id = params["friendId"].to_i
+    friend_id = params["uniqueId"].to_i
     shared_basket = SharedBasket.find(friend_id)
 
     #include the new info being sent in from the extension
     shared_basket.shared_links.create(sender_id: current_user.id,
                                       url: params["url"],
-                                      title:,
-                                      description:,
-                                      image: )
+                                      title: params["pageInfo"]["title"],
+                                      description: params["pageInfo"]["description"],
+                                      image: params["pageInfo"]["image"])
     redirect_to root_url
   end
 
