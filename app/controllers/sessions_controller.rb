880638875a234b42023fda98dbd96538f1ecf95d
Onaracs <brendan.scarano@gmail.com>
2014-09-10 22:53:38 -0400

    added 3 starting folders for when users first login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
 
     SharedBasket.where(user_id: user.id).first_or_create
+    Folder.where(name: "Read Later", user_id: user.id).first_or_create
+    Folder.where(name: "Funny Links", user_id: user.id).first_or_create
+    Folder.where(name: "New Music", user_id: user.id).first_or_create
     redirect_to root_path
   end
 
