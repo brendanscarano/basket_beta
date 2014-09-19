e8a8c09d8ead48e0f4968229e87e064c1a4fd9de
Onaracs <brendan.scarano@gmail.com>
2014-09-19 08:37:27 -0400

    when share button is clicked, section only slides down for that specific section

--- app/assets/javascripts/dragDrop.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function(){
-//   $('.fa-share').draggable({
-//     containment: 'document',
-//     cursor: 'move',
-//     cursorAt: { botton: 10},
-//     helper: 'clone'
-//   });
-// });
--- app/assets/javascripts/share.js
+++ app/assets/javascripts/share.js
@@ -1,25 +1,28 @@
 $(document).ready(function(){
   // var html = "<%= escape_javascript(render('folders/share')) %>";
-  $('.fa-share').on('click', function(){
-    $('.save-share-div').slideDown("medium", function(){
-      // $('.save-share-div-container').html(html)
-    })
+  $('.link-share').on('click', function(){
+    id = $(this).attr('data');
+    $('.save-share-div.' + id).slideDown("medium", function(){})
+
+    ajaxCalls(id);
+    
+  });
+});
+
+function ajaxCalls(folder_id){
+    console.log(folder_id);
 
     $.ajax({
       type: "GET",
       url: "/users_folders"
     }).done(function(response){
-      console.log(response);
-      $('.basket-list').append(response);
+      $('.basket-list.' + folder_id).append(response);
     })
 
     $.ajax({
       type: "GET",
       url: "/users_friends"
     }).done(function(response){
-      console.log(response);
-      $('.friend-list-wrapper').append(response);
+      $('.friend-list-wrapper.' + folder_id).append(response);
     })
-    
-  });
-});
+}
