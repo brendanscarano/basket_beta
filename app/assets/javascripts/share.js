4a30cbc23cf2435c05292958e4d4c447f7b259f5
Onaracs <brendan.scarano@gmail.com>
2014-09-20 09:38:12 -0400

    Users now have a confirmation message for deleting a link Fixed the issue where if multiple garbage cans were clicked, the site would delete the wrong one Now if multiple cans are clicked the other ones hide themselves and only one can be shown at a time

--- app/assets/javascripts/share.js
+++ app/assets/javascripts/share.js
@@ -1,28 +1,33 @@
-$(document).ready(function(){
-  // var html = "<%= escape_javascript(render('folders/share')) %>";
-  $('.link-share').on('click', function(){
-    id = $(this).attr('data');
-    $('.save-share-div.' + id).slideDown("medium", function(){})
+// $(document).ready(function(){
+//   // var html = "<%= escape_javascript(render('folders/share')) %>";
+//   $('.link-share').draggable({
+//     containment: 'window',
+//     helper: 'clone'
+//   })
 
-    ajaxCalls(id);
+//   $('.link-share').on('click', function(){
+//     id = $(this).attr('data');
+//     $('.save-share-div.' + id).slideDown("medium", function(){})
+
+//     ajaxCalls(id);
     
-  });
-});
+//   });
+// });
 
-function ajaxCalls(folder_id){
-    console.log(folder_id);
+// function ajaxCalls(folder_id){
+//     console.log(folder_id);
 
-    $.ajax({
-      type: "GET",
-      url: "/users_folders"
-    }).done(function(response){
-      $('.basket-list.' + folder_id).append(response);
-    })
+//     $.ajax({
+//       type: "GET",
+//       url: "/users_folders"
+//     }).done(function(response){
+//       $('.basket-list.' + folder_id).append(response);
+//     })
 
-    $.ajax({
-      type: "GET",
-      url: "/users_friends"
-    }).done(function(response){
-      $('.friend-list-wrapper.' + folder_id).append(response);
-    })
-}
+//     $.ajax({
+//       type: "GET",
+//       url: "/users_friends"
+//     }).done(function(response){
+//       $('.friend-list-wrapper.' + folder_id).append(response);
+//     })
+// }
